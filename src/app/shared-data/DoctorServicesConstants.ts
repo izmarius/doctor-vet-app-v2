c77628a7e026e04c38955011a76ff8cdd8f40f7b
pausanionut <pausan@media-saturn.com>
2021-07-06 17:56:55 +0300

    Add list of services

---
+++ src/app/shared-data/DoctorServicesConstants.ts
@@ -0,0 +1,49 @@
+export const DOCTOR_SERVICES = {
+
+  GENERAL: {
+    serviceName: 'General',
+    services: ['Consultatie generala', 'Control periodic', 'Consultatie la domiciliu', 'Vaccinare', 'Insamantare artificiala caini']
+  },
+  DATA: {
+    serviceName: 'DATE',
+    services: ['Carnet de sanatate', 'Creare pasaport', 'Implantare microcip']
+  },
+  IMAGERY: {
+    serviceName: 'Imagistica',
+    services: ['Ecografie', 'Ecocardografie', 'Radiografie']
+  },
+  ANALYZES: {
+    serviceName: 'Analize',
+    services: ['Analize sange', 'Analize scaun', 'Analize urina']
+  },
+  DENTISTRY: {
+    serviceName: 'Stomatologie',
+    services: ['Detartraj', 'Extractii(molari,premolari,canini,incisivi)']
+  },
+  PHYSIOTHERAPY: {
+    serviceName: 'Fizioterapie',
+    services: ['Fizioterapie prin laser', 'Fizioterapie prin magneti', 'Fizioterapie in apa']
+  },
+  CARE: {
+    serviceName: 'TOALETAJ',
+    services: ['Toaletaj canin si felin']
+  },
+  SURGERY: {
+    serviceName: 'Chirurgie',
+    services: ['Chirurgia cavitatii bucale', 'Sterilizare', 'Cezariana', 'Fracturi', 'Amputare membru', 'Chirurgie articulara', 'Chirurgie cardiologica']
+  },
+  URGENT: {
+    serviceName: 'Urgente',
+    services: ['Urgente']
+  },
+  HOSPITALIZATION: {
+    serviceName: 'Spitalizare',
+    services: ['Terapie intensiva si postoperatorie', 'Internare perioada de convalescenta', 'Internare in regim de pensiune']
+  },
+  PHARMACY: {
+    serviceName: 'Farmacie si suplimente alimentare',
+    services: ['Hrana și diete speciale', 'Suplimente', 'Unguente si sampoane medicamentoase', 'Medicamente']
+  }
+
+
+}
