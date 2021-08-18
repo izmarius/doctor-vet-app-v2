9b58039a4d7bcde2d30f6f3f0535c0d487a11f77
pausanionut <pausan@media-saturn.com>
2021-08-18 10:33:08 +0300

    edit media queries

--- src/app/ui/shared/navbar/navbar.component.ts
+++ src/app/ui/shared/navbar/navbar.component.ts
@@ -28,7 +28,6 @@ export class NavbarComponent implements OnInit {
 
   openAppointmentsModal(): void {
     const dialogRef = this.dialog.open(DoctorAppointmentModalComponent, {
-      width: '25%',
       height: '40rem',
       data: null
     });
