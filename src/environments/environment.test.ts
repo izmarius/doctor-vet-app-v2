8fd1cdf26418b90e131fc6d244fb4ad9051ec6a3
pausanionut <pausan@media-saturn.com>
2021-12-22 16:42:29 +0200

    Add dev environment

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyBKTrImKfVJYXTlZmcj4SagdrNxH5VEfkM",
+    authDomain: "doctor-vet-test.firebaseapp.com",
+    projectId: "doctor-vet-test",
+    storageBucket: "doctor-vet-test.appspot.com",
+    messagingSenderId: "127134271721",
+    appId: "1:127134271721:web:85680eb85d5a5280194878"
+  },
+  production: false
+};
